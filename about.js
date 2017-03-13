b6337bce0dea070fe8228429642421b2da6e021b
Kemar Galloway <kemargalloway@gmail.com>
2017-03-12 21:25:42 -0500

    Update about.js

--- about.js
+++ about.js
@@ -1,4 +1,9 @@
-  $('.carousel.carousel-slider').carousel({fullWidth: true});
+$(document).ready(function () {
+    $(".dropdown-button").dropdown();
+    $('.slider').slider({ full_width: true, height: 500 });
+
+
+});
    $(document).ready(function(){
     $('.collapsible').collapsible();
   });
